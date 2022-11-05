03d61b732cccdba746d717e5978e02babaecf97e
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-11-05 11:33:05 +0100

    chore: tox release pipeline

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Kevin Maik Jablonka"
 author = "Kevin Maik Jablonka"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.8.0-dev"
 
 
 # -- General configuration ---------------------------------------------------
