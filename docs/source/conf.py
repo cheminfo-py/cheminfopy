1ff381d945e79dd1cfb913ad04e37d57de074d01
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-11-05 11:29:39 +0100

    Bump version: 0.0.1-dev → 0.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Kevin Maik Jablonka"
 author = "Kevin Maik Jablonka"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1-dev"
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
